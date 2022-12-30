86bdbb660c6fa4a8429c20f3f33a82dee39cf737
vovkaWeb <vova77557@gmail.com>
2022-12-30 23:15:12 +0200

    update

--- src/components/employers-list/emloyers-list.js
+++ src/components/employers-list/emloyers-list.js
@@ -1,14 +1,14 @@
-/** @format */
-
 import EmployeesListItem from "../employers-list-item/employers-list-item";
 import "./employers-list.css";
 
 const EmployeesList = ({ data }) => {
-  const elements = data.map((item) => <EmployeesListItem {...item}/>);
+  const elements = data.map((item) => {
+  const {id, ...itemProps} = item;
+  return(
+  <EmployeesListItem  key={id} {...itemProps}/>)});
 
   return (
-    <ul
-      className="app-list list-group">
+    <ul className="app-list list-group">
     {elements}
     </ul>
   );
